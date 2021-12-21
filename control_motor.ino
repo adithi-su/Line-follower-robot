7912738152fef78992adfcc1230bcf5a0c49745e
Adithi <63908022+adithi-su@users.noreply.github.com>
2021-12-22 00:30:05 +0530

    Add files via upload

--- control_motor.ino
+++ control_motor.ino
@@ -1,12 +1,15 @@
 void setup() {
+// motor positive - digital 10, negative - digital 11
 pinMode(11, OUTPUT); 
 pinMode(10, OUTPUT);
 }
 
 void loop() {
+// to run clockwise - 10 (connected to positive terminal) - LOW, 11( to negative terminal) - HIGH
 digitalWrite(10, LOW);
 digitalWrite(11, HIGH);
 delay(3000); //wait for 3 sec
+// anticlockwise rotation - 
 digitalWrite(10, HIGH);
 digitalWrite(11, LOW);
 delay(3000); //wait for 3 sec
